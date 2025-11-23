64a43942a4851b17ecf8dd71ce55165ecbe5f22c
Camila Cotapos <camilacotaposmz@gmail.com>
2025-11-22 21:21:20 -0300

    xd

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import React from 'react'; // Importante para evitar el error de pantalla gris
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
