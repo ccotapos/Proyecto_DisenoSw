b1993bc9e489baeaab1db51b89b45e230fdc87f1
Camila Cotapos <camilacotaposmz@gmail.com>
2025-11-28 20:46:40 -0300

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api'; 
+const API_URL = 'https://proyecto-backend-xhpk.onrender.com'; 
 
 const api = axios.create({
   baseURL: API_URL,
